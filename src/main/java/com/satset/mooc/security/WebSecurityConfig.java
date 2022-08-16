153d9e16cc4d9dff54f01d11da3e13f2834673fd
Evan Aurelrius <evanserk@gmail.com>
2022-08-16 09:30:26 +0700

    Add auth config for all routes to fully authenticate

--- src/main/java/com/satset/mooc/security/WebSecurityConfig.java
+++ src/main/java/com/satset/mooc/security/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig {
                 .and().authorizeRequests()
                 .antMatchers("/api/login").permitAll()
                 .antMatchers("/api/register").permitAll()
-                .antMatchers("/api/**").permitAll()
+                .antMatchers("/api/**").fullyAuthenticated()
                 .anyRequest().authenticated();
         http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
