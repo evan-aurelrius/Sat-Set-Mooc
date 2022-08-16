566e4c1ec9054e3c2157486475bdb352e79a43c1
Adnan Al-farizi Rahmat <42133636+alfa000@users.noreply.github.com>
2022-08-16 23:59:03 +0700

    fix typo

--- src/main/java/com/satset/mooc/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/satset/mooc/security/jwt/AuthTokenFilter.java
@@ -32,7 +32,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
         try {
             response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTION");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, X-Requested-With, remember-me");
 
